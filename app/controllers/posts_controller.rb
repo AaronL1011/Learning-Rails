89be72665d331815ad1f8deba60affcad101221c
Aaron Lewis <aaron.lewis1011@gmail.com>
2020-04-06 17:17:53 +1000

    Completed blog application

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+    http_basic_authenticate_with name: "aaron", password: "123123", except: [:index, :show]
+
     def index
         @posts = Post.all
     end
@@ -21,6 +23,26 @@ class PostsController < ApplicationController
             render 'new'
         end
 
+    end
+
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def update
+        @post = Post.find(params[:id])
+        if(@post.update(post_params))
+            redirect_to @post
+            else
+                render 'edit'
+            end
+    end
+
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+
+        redirect_to posts_path
     end
 
     private def post_params
